5c2d8f23cdfd39e76a0f6ae12dd333b1d09c0c9c
Pavel Saman <pavelsam@centrum.cz>
2020-12-26 15:06:07 +0100

    inputs.js

---
+++ inputs.js
@@ -0,0 +1,32 @@
+
+(function() {
+    var inputs = document.querySelectorAll('input');
+    var fields = [];
+
+    inputs.forEach((input, index) => {
+        console.group(index);
+        console.log(input);
+
+        console.table({
+            type: input.type,
+            name: input.name || undefined,
+            id: input.id || undefined,
+            class: input.className || undefined,
+            hidden: input.hidden,
+            required: input.required || input.getAttribute('data-val-required') ? true : false,
+            min: input.min || undefined,
+            max: input.max || undefined,
+            minLength: input.minLength || undefined,
+            maxLength: input.maxLength || undefined,
+            pattern: input.pattern || input.getAttribute('data-val-regex-pattern') || undefined,
+            placeholder: input.placeholder || undefined,
+            value: input.value || undefined,
+            form: input.form ? true : false
+        });
+        
+        console.groupEnd();
+        fields = [];
+    });
+
+})();
+
